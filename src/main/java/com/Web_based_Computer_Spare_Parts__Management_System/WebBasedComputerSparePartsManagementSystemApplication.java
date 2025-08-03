e5929f2bc2ca6c33f4d935be905048c0828616aa
Atheek-Fareez <Atheekfareez47@gmail.com>
2025-08-03 10:24:16 +0530

    Initial Commit

---
+++ src/main/java/com/Web_based_Computer_Spare_Parts__Management_System/WebBasedComputerSparePartsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Web_based_Computer_Spare_Parts__Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedComputerSparePartsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBasedComputerSparePartsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Web_based_Computer_Spare_Parts__Management_System/WebBasedComputerSparePartsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Web_based_Computer_Spare_Parts__Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedComputerSparePartsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
